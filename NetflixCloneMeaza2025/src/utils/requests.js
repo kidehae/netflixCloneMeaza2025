43ec399402c4d2b262a22a5637afbc9a30dac2e8
kidehae <149243635+kidehae@users.noreply.github.com>
2025-06-21 10:44:52 +0300

    connecting to api

---
+++ NetflixCloneMeaza2025/src/utils/requests.js
@@ -0,0 +1,21 @@
+apiKey = import.meta.env.VITE_API_KEY;
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  fetchSciFiMovies: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  fetchWesternMovies: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchTvShows: `/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc`,
+  fetchPopularMovies: `/movie/popular?api_key=${API_KEY}&language=en-US`,
+  fetchUpcomingMovies: `/movie/upcoming?api_key=${API_KEY}&language=en-US`,
+  fetchNowPlayingMovies: `/movie/now_playing?api_key=${API_KEY}&language=en-US`,
+  fetchPopularPeople: `/person/popular?api_key=${API_KEY}&language=en-US`,
+};
